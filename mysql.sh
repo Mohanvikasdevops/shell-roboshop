b8973215e9f4f3d994ae78c3c748028a9996586f
Mohanvikas <vikas.gonugunta@gmail.com>
2026-03-18 22:57:00 +0530

    actions

--- mysql.sh
+++ mysql.sh
@@ -27,10 +27,10 @@ VALIDATE(){
     fi
 }
 
-dnf install mysql-server -y
+dnf install mysql-server -y &>>$LOGS_FILE
 VALIDATE $? "Installed MySQL server"
 
-systemctl enable mysqld
+systemctl enable mysqld &>>$LOGS_FILE
 systemctl start mysqld  
 VALIDATE $? "Enable  and start mysql"
 
