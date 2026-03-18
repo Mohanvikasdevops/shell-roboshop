3eb8e6b352b15cba895fbf483c3266588cedb4f7
Mohanvikas <vikas.gonugunta@gmail.com>
2026-03-18 21:11:54 +0530

    actions

---
+++ cart.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+cartID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.109v.store
+cart_HOST=cart.109v.store
+REDIS_HOST=redis.109v.store
+CATALOGUE_HOST=catalogue.109v.store
+
+if [ $cartID -ne 0 ]; then
+    echo -e "$R Please run this script wiht root cart access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf module disable nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Disabling NodeJs default version"
+
+dnf module enable nodejs:20 -y &>>$LOGS_FILE
+VALIDATE $? "Enabling NodeJs version 20"
+
+dnf install nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Installing NodeJs"
+
+id roboshop &>>$LOGS_FILE
+if [ $? -ne 0 ]; then   
+    cartadd --system --home /app --shell /sbin/nologin --comment "roboshop system cart" roboshop &>>$LOGS_FILE
+    VALIDATE $? "Creating system cart"
+else
+    echo -e "Roboshop cart already exist .... $Y SKIPPING $N"
+fi
+
+mkdir -p /app 
+VALIDATE $? "Creatng app directory"
+
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>>$LOGS_FILE
+VALIDATE $? "Downloading cart Code"
+
+cd /app
+VALIDATE $? "Moving to app directory" 
+
+rm -rf /app/* &>>$LOGS_FILE
+VALIDATE $? "Removing existing code"
+
+unzip /tmp/cart.zip &>>$LOGS_FILE
+VALIDATE $? "Unzip cart code"
+
+npm install &>>$LOGS_FILE
+VALIDATE $? "Installing dependencies"
+
+cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service &>>$LOGS_FILE
+VALIDATE $? "Created systemctl service"
+
+systemctl daemon-reload
+systemctl enable cart &>>$LOGS_FILE
+systemctl start cart
+VALIDATE $? "Starting and enabling cart"
+
