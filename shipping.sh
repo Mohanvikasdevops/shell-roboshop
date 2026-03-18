2f1c38fe70b527e1b469a1006d99b16cc393af5d
Mohanvikas <vikas.gonugunta@gmail.com>
2026-03-18 23:37:26 +0530

    actions

--- shipping.sh
+++ shipping.sh
@@ -66,17 +66,19 @@ VALIDATE $? "Moving and Renaming shipping"
 cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service &>>$LOGS_FILE
 VALIDATE $? "Created systemctl service"
 
-systemctl daemon-reload
-systemctl enable shipping &>>$LOGS_FILE
-systemctl start shipping
-VALIDATE $? "Starting and enabling shipping"
-
-
 dnf install mysql -y &>>$LOGS_FILE
 VALIDATE $? "Installing MySQL"
 
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql 
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
+if[ $? -ne 0 ]; then
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
+    VALIDATE $? "Loaded data into MySQL"
+else
+    echo -e "data is already loaded ... $Y SKIPPING $N"
+fi
 
-systemctl restart shipping
+systemctl enable shipping &>>$LOGS_FILE
+systemctl start shipping
+VALIDATE $? "Starting and enabling shipping"
